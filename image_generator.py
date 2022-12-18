a3f4d71a873d192fdf5861d1c04db9a532dffabe
De3Zy <89665316+DominikZzZ@users.noreply.github.com>
2022-12-18 17:55:42 +0100

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -12,20 +12,28 @@ import openai
 # Then click on your profile (upper right corner) and enter "View API keys" and generate a new API key
 openai.api_key = "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"
 
+# Getting input from the user
+# Here we ask about the subject of the picture
 user_prompt = input("Please enter the topic of the image: ")
+# And here for the number of images
+# The number you enter must be a positive integer
 user_range = int(input("Please enter the number of generated images: "))
 
-for i in range(0, user_range):
-    print(f"\nGenerating image {i + 1}...")
+try:
+    for i in range(0, user_range):
+        print(f"\nGenerating image {i + 1}...")
 
-    response = openai.Image.create(
-        prompt=user_prompt,
-        n=1,                          
-        size="256x256" # all possible sizes: "256x256", "512x512", "1024x1024"
-    )
-    image_url = response["data"][0]["url"]
+        response = openai.Image.create(
+            prompt=user_prompt,
+            n=1,                          
+            size="256x256" # all possible sizes: "256x256", "512x512", "1024x1024"
+        )
+        image_url = response["data"][0]["url"]
 
-    print(f"{i + 1}: {image_url}\n")
-    # Opening the generated image in a browser
-    # You can use "webbrowser.open_new_tab()" as well
-    webbrowser.open_new_tab(image_url)
+        print(f"{i + 1}: {image_url}\n")
+        # Opening the generated image in a browser
+        # You can use "webbrowser.open_new_tab()" as well
+        webbrowser.open_new_tab(image_url)
+        
+except: openai.error.OpenAIError as e:
+        print(f"ERROR: {e}\nHTTP STATUS: {e.http_status}")
