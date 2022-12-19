655eddb9e01320b1e065cc79c093b7fb88684811
De3Zy <89665316+DominikZzZ@users.noreply.github.com>
2022-12-19 20:17:32 +0100

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -6,7 +6,7 @@ import openai
 
 
 # Here you have to enter your own api key
-# openai.api_key = "your api key"
+# openai.api_key = "api key"
 # To find your api key you need to go to https://beta.openai.com/overview, then sign up / log in
 # Then click on your profile (upper right corner) and enter "View API keys" and generate a new API key
 openai.api_key = "your api key"
@@ -18,6 +18,10 @@ user_prompt = input("Please enter the topic of the image: ")
 # The number you enter must be a positive integer
 user_range = int(input("Please enter the number of generated images: "))
 
+# image resolution
+# All possible sizes: "256x256", "512x512", "1024x1024"
+image_res = "512x512"
+
 try:
     for i in range(0, user_range):
         print(f"\nGenerating image {i + 1}...")
@@ -25,7 +29,7 @@ try:
         response = openai.Image.create(
             prompt=user_prompt,
             n=1,                          
-            size="512x512" # all possible sizes: "256x256", "512x512", "1024x1024"
+            size=image_res
         )
         image_url = response["data"][0]["url"]
 
@@ -36,3 +40,4 @@ try:
 
 except openai.error.OpenAIError as e:
     print(f"ERROR: {e}\nHTTP STATUS: {e.http_status}")
+
